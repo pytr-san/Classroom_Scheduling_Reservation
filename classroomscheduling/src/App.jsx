d013408d6a8ae121650ba988d9d4cb30a4ae648f
pytr-san <speterjeorge@gmail.com>
2025-03-14 03:52:42 +0800

    Navigational bar added and pages folder and files

--- classroomscheduling/src/App.jsx
+++ classroomscheduling/src/App.jsx
@@ -1,15 +1,13 @@
-import Header from "./components/Header"
+//import Header from "./components/Header"
 import Aside from "./components/Aside";
 import Footer from "./components/Footer";
-import Card from "./components/Card";
+
 function App() {
 
     return(
         <>
-        <Header/>
         <Aside/>
         <Footer/>
-        <Card/>
         </>
     );
 }
--- classroomscheduling/src/components/Aside.jsx
+++ classroomscheduling/src/components/Aside.jsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./Navbar.css"; // External CSS file
+import spistlogo from "../assets/logo1.png"
+import accessslogo from "../assets/coppp.png"
 
-function Aside(){
+function Navbar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    return(
+  return (
+    <>
+       {/* Navbar */}
+       <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
+        <button 
+          className="btn btn-light me-3" 
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
+          <i className="bi bi-list" style={{ fontSize: "1.5rem"}}></i>
+        </button>
+        <img src={spistlogo} alt="spist logo" className="img-icon me-3" />
+        <a className="navbar-brand" href="#">Southern Philippines Institute of Science and Technology</a>
+      </nav>
 
-     <aside>
-            <div className="sidebar">
-                    <ul>
-                        <li><a href="#"><span className="material-symbol-outlined">home</span><span>Home</span></a></li>
-                        <li><a href="#"><span className="material-symbol-outlined">classroom</span><span>Classroom</span></a></li>
-                        <li><a href="#"><span className="material-symbol-outlined">course</span><span>Course</span></a></li>
-                        <li><a href="#"><span className="material-symbol-outlined">settings</span><span>Settings</span></a></li>
-                        <p>Welcome to access</p>
-                    </ul>
-            </div>
-     </aside>
-        );
-
+      {/* Sidebar */}
+      <div 
+        className={`sidebar bg-light ${isSidebarOpen ? "open" : "closed"}`} 
+        onMouseEnter={() => setIsSidebarOpen(true)}
+        onMouseLeave={() => setIsSidebarOpen(false)}
+      >
+        <nav className="nav flex-column p-3">
+          <a className="nav-link" href="#"><img src={accessslogo} alt="access logo" className="logo me-2" /> <span>Home</span></a>
+          <a className="nav-link" href="#"><i className="bi bi-people me-2"></i> <span>Classroom</span></a>
+          <a className="nav-link" href="#"><i className="bi bi-book me-2"></i> <span>Course</span></a>
+          <a className="nav-link" href="#"><i className="bi bi-gear me-2"></i> <span>Settings</span></a>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default Aside
+export default Navbar;
